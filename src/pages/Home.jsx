460cf0766d803de42dcef116e847d2f698bf0cc3
Murutu <murutupeter@gmail.com>
2023-05-08 09:57:10 +0300

    chore: added logout functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import React from 'react';
+import { useCookies } from 'react-cookie';
+import { useAuthValue } from '../context/Auth/AuthWrapper';
+import { actionTypes } from '../context/Auth/AuthReducer';
+import { useNavigate } from 'react-router-dom';
+
+
+import { Box, Button } from '@chakra-ui/react';
+
 
 const Home = () => {
+    const [,, removeCookie] = useCookies(["jwt"]);
+    const [ ,dispatch] = useAuthValue();
+    const navigate = useNavigate();
+
+    const logout = () => {
+        removeCookie("jwt");
+        dispatch({ type: actionTypes.SET_TOKEN, value: null});
+        navigate("/login");
+    }
     return (
-    <div>Home</div>
+    <Box>
+        <Button onClick={logout}> Log out </Button>
+    </Box>
     )
 }
 
